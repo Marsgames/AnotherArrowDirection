d3770d6349f2158aa55adad24dc45ce1eb6946d9
Marsgames <raphdaumas@hotmail.fr>
2024-07-25 09:29:50 +0200

    Initial Commit

---
+++ AnotherArrowDirection/AnotherArrowDirectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherArrowDirectionApp.swift
+//  AnotherArrowDirection
+//
+//  Created by Raphaël Daumas on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnotherArrowDirectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
